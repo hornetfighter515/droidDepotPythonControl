49c69ff74c11e83591e994cdec6c413ae204418e
hornet <hornetfighter515@protonmail.com>
2022-10-09 21:36:30 -0400

    Added keyboard functionality for existing features

---
+++ keyboard_control.py
@@ -0,0 +1,53 @@
+import droid
+import asyncio
+from time import sleep
+from random import randrange
+
+
+async def play_sound(d):
+    # number = input("Please input a number of which sound to play. ")
+    number = randrange(0,5)
+    await d.play_sound(f"0{number}","00")
+
+runningDroid = True
+
+async def quit(d):
+    global runningDroid
+    runningDroid = False
+
+async def main():
+    # first, get droid
+    d = await droid.discoverDroid(retry=True)
+
+    commands = {
+            #"w":forward,
+            #"a": left,
+            #"s": backward,
+            #"d": right,
+            #"e": rotate counter-clockwise,
+            #"r": rotate clockwise,
+            #"f": special effect
+            "n":play_sound,
+            "q":quit
+            }
+    try:
+        await d.connect()
+        global runningDroid
+        while runningDroid:
+            # next, await input
+            command = input("Please input a command.")
+            # next, parse that command
+            c = command.lower()[0:1]
+            if c in commands.keys():
+                await commands[c](d)
+            else:
+                print("Command does not exist.")
+
+
+            sleep(0.2)
+
+    finally:
+        await d.disconnect()
+
+if __name__ == "__main__":
+    asyncio.run(main())
